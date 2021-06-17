fa7e8b8601944d3167ebd655d8d60526e60014cc
facundob <facundob@nimble.la>
2021-06-17 17:08:22 -0300

    FIX: UI tunnings

--- SuperHeros/HeroCollection/HeroCollectionViewCell.swift
+++ SuperHeros/HeroCollection/HeroCollectionViewCell.swift
@@ -10,10 +10,20 @@ import UIKit
 class HeroCollectionViewCell: UICollectionViewCell {
     @IBOutlet var name: UILabel!
     @IBOutlet var image: UIImageView!
+    @IBOutlet var bannerView: UIView!
     
     func configure(hero: Hero) {
         name.text = hero.name
         image.setImageFromUrl(hero.images.smallImageURL, completion: nil)
         contentView.layer.cornerRadius = 5
+        contentView.layer.borderWidth = 1
+        contentView.layer.borderColor = UIColor.white.cgColor
+        bannerView.layer.borderWidth = 1
+        bannerView.layer.borderColor = UIColor.white.cgColor
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        image.image = nil
     }
 }
